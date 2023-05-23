80ae84cfd1fa87005dd6dafb4dc8eeec32216a26
ElementoBorzi <113427648+ElementoBorzi@users.noreply.github.com>
2023-05-24 01:56:20 +0300

    Added support for EN localisation

--- core.lua
+++ core.lua
@@ -1,12 +1,15 @@
 local eventHandler;
 local SERVER_RESTART_FORCE = "Перезагрузка через";
+local SERVER_RESTART_FORCE_EN = "Restart in";
 
-local FONT, FONTSIZE, FONTCOLOR = "Fonts\\Arialn.ttf", 22, {0, 1, 1};
+local FONT, FONTSIZE = "Fonts\\Arialn.ttf", 22;
+local FONTCOLOR = {0, 1, 1}; -- Голубой цвет (RGB: 0, 255, 255)
+local FONTCOLOR_EN = {1, 1, 0}; -- Желтый цвет (RGB: 255, 255, 0)
+
 local g_MsgSync;
 
--- /* hud message */
 do
-    local function showsynchudmsg(duration, time, justify, width, height)
+    local function showsynchudmsg(duration, time, justify, width, height, language)
         local hud = CreateFrame("ScrollingMessageFrame", nil, UIParent);
         hud:SetMaxLines(1);
         hud:SetFadeDuration(duration);
@@ -16,18 +19,17 @@ do
         hud:SetFont(FONT, FONTSIZE);
         hud:SetShadowColor(0, 0, 0, 1);
         hud:SetShadowOffset(1, -1);
-        hud:SetTextColor(unpack(FONTCOLOR));
+        hud:SetTextColor(unpack(language == "ru" and FONTCOLOR or FONTCOLOR_EN));
         hud:SetPoint("CENTER", UIParent, "CENTER", 0, 240);
         hud:SetFrameStrata("HIGH");
-        hud:Hide(); -- скрыть фрейм при инициализации
+        hud:Hide();
 
         return hud;
     end
 
-    g_MsgSync = showsynchudmsg(4, 5, "CENTER", 620, FONTSIZE);
+    g_MsgSync = showsynchudmsg(4, 5, "CENTER", 620, FONTSIZE, "ru");
 end
 
--- /* fire event */
 do
     eventHandler = CreateFrame("Frame");
     eventHandler:RegisterEvent("CHAT_MSG_SYSTEM");
@@ -40,12 +42,10 @@ do
 end
 
 function eventHandler:CHAT_MSG_SYSTEM(_, msg)
-    if string.find(msg, SERVER_RESTART_FORCE) then -- использовать string.find вместо gmatch
+    if string.find(msg, SERVER_RESTART_FORCE) or string.find(msg, SERVER_RESTART_FORCE_EN) then
         g_MsgSync:AddMessage(msg);
-        g_MsgSync:Show(); -- показать фрейм при обнаружении сообщения
+        g_MsgSync:Show();
     end
 end
 
--- боже это невероятно
-
-print("|cffc41f3bRestarter Notice|r: |cffffff00by Woody#6606|r")
+print("|cffc41f3bRestarter Notice|r: |cffffff00by Woody#6606|r");
